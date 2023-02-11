17bcebc45d7c37c02721a4a07db53d84eaa677cc
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-02-11 09:43:22 +0700

    up

---
+++ bai2/exercise/E1.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE QuanLiVatTu;
+use QuanLiVatTu;
+CREATE TABLE VatTu(
+    TenVatTu VARCHAR(20),
+    MaVatTu VARCHAR(10) PRIMARY KEY
+);
+CREATE TABLE PhieuXuat(SoPX int PRIMARY KEY, NgayXuat DATE);
+CREATE TABLE PhieuNhap(soPN INT PRIMARY KEY, NgayNhap DATE);
+CREATE TABLE DonDatHang(
+    SoDatHang INT PRIMARY KEY,
+    NgayDatHang DATE
+);
+CREATE TABLE NhaCungCap(
+    MaNhaCungCap INT PRIMARY KEY not null,
+    TenNhaCungCap VARCHAR(30),
+    DiaChiNhaCungCap VARCHAR(50),
+    SdtNhaCungCap VARCHAR(15)
+   
+);
+CREATE TABLE ChiTietPhieuXuat(
+    SoLuongXuat INT,
+    DGXuat INT,
+    SoPX INT,
+    MaVatTu VARCHAR(10),
+    FOREIGN KEY (SoPX) REFERENCES PhieuXuat(SoPX),
+    FOREIGN KEY (MaVatTu) REFERENCES VatTu(MaVatTu)
+);
+CREATE TABLE ChiTietPhieuNhap(
+    DGNhap INT,
+    SoLuongNhap INT,
+    MaVatTu VARCHAR(10),
+    SoPN INT,
+    FOREIGN KEY (MaVatTu) REFERENCES VatTu(MaVatTu),
+    FOREIGN KEY (SoPN) REFERENCES PhieuNhap(SoPN)
+);
+CREATE TABLE ChiTietDonDatHang(
+    SoDatHang INT,
+    MaVatTu VARCHAR(10),
+    FOREIGN KEY (MaVatTu) REFERENCES VatTu(MaVatTu),
+    FOREIGN KEY (SoDatHang) REFERENCES DonDatHang(SoDatHang)
+);
