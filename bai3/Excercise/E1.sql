d13c62c59fa8c2fb98e378b6c71616e1e092fc82
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-02-15 18:56:41 +0700

    e2

--- bai3/Excercise/E1.sql
+++ bai3/Excercise/E1.sql
@@ -2,6 +2,9 @@ use quanlisinhvien;
 select * from student where StudentName like 'h%';
 SELECT ClassName, StartDate from class where month(StartDate) = '12';
 select *  from subject where Credit > 5  ;
+update class
+set ClassID = '2'
+where ClassName = 'hung';
 select student.StudentName, subject.SubName , mark 
 from student,mark,subject 
 order by mark desc, StudentName
--- bai3/Excercise/E2.sql
+++ bai3/Excercise/E2.sql
@@ -2,8 +2,28 @@ use quanlybanhang;
 insert into customer
 values (1,'Minh Quan',10),(2,'Ngoc Oanh',20),
 (3,'Hong Ha',50);
-insert into `order`
-values (1,1,3-21-2006,null),
-(2,2,3-23-2006),
-(3-1-3-16-2006,null);
-select * from `order`;
+insert into `order`(cID,oDate,oTotalPrice)
+values (1,'2006-03-21',null),
+(2,'2006-03-23',null),
+(1,'2006-03-16',null);
+
+insert into product (pName,pPrice)
+values ('May Giat',3),('Tu Lanh',5),('Dieu Hoa',7),
+('Quat',1),('Bep Dien',2);
+
+insert orderdetail (odQTY)
+values (3),(7),(2),(1),(8),(4),(3);
+
+select oID,oDate,oTotalPrice from `order` ;
+select customer.cName ,product.pName, `order`.cID,customer.cID  from customer,`order`,product;
+select * from customer c
+join `order` o on c.cid = o.cid;
+
+select cID,cName 
+from customer
+where not exists ( select cID from `order` 
+where cID = customer.cID);
+
+
+
+
