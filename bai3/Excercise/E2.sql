43cf4802f1baecc7f840a1e7e9717008ceaa0496
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-02-15 17:24:25 +0700

    a

---
+++ bai3/Excercise/E2.sql
@@ -0,0 +1,9 @@
+use quanlybanhang;
+insert into customer
+values (1,'Minh Quan',10),(2,'Ngoc Oanh',20),
+(3,'Hong Ha',50);
+insert into `order`
+values (1,1,3-21-2006,null),
+(2,2,3-23-2006),
+(3-1-3-16-2006,null);
+select * from `order`;
